87667af3dfc7c0457d27c7d48188d4c6a1d53096
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-08-27 18:00:13 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const app = express();
+
+// Connect to Database
+const connectDB = require("./db/connect");
+
+const authenticate = require("./middleware/auth");
+
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 10, // Limit each IP to 5 create account requests per `window` (here, per hour)
+    message:
+      "Too many accounts created from this IP, please try again after an hour",
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// extra packages
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticate, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
